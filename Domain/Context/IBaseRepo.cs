9cf426d13c557fdeaffa6eaa1c1e21c8bdf488b4
Pontus Pettersson <pontusp02j@gmail.com>
2023-10-17 16:29:06 +0200

    added in domain layer here

---
+++ Domain/Context/IBaseRepo.cs
@@ -0,0 +1,10 @@
+namespace GridStatusHub.Domain.Context.BaseRepo {
+    public interface IBaseRepo<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<int> InsertAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
