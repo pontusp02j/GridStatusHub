b73de9e3616ca022ad922456201ed801e4036f43
Pontus Pettersson <pontusp02j@gmail.com>
2023-10-22 14:44:46 +0200

    remove data anotation for dapper have a bug and will sometimes struggling with it

--- Domain/Requests/GridSystem/GridSystemRequest.cs
+++ Domain/Requests/GridSystem/GridSystemRequest.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using GridStatusHub.Domain.Requests.GridCell;
 
 namespace GridStatusHub.Domain.Requests.GridSystem 
@@ -6,8 +5,6 @@ namespace GridStatusHub.Domain.Requests.GridSystem
     public class GridSystemRequest
     {
         public int Id { get; set; }  
-        [StringLength(15, ErrorMessage = "Name cannot be longer than 15 characters.")]
-        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Only alphanumeric characters are allowed.")]
         public string Name { get; set; } = string.Empty; 
         public DateTime? EstablishmentDate { get; set; } 
         public ICollection<GridCellRequest>? GridCells { get; set; }
